0496f05795c3fdd152513714f2883acbc56b3022
MuhammadArslanKhan96 <m.arslan2201@gmail.com>
2024-04-17 12:49:55 +0500

    chore initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import ScrollToTop from "@/components/ScrollToTop";
+import { ThemeProvider } from "next-themes";
+import "../styles/index.css";
+import "../styles/prism-vsc-dark-plus.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html suppressHydrationWarning={true} className="!scroll-smooth" lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+
+      <body>
+        <ThemeProvider
+          attribute="class"
+          enableSystem={false}
+          defaultTheme="light"
+        >
+          <Header />
+          {children}
+          <Footer />
+          <ScrollToTop />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
